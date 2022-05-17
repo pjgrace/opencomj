ca38e9f9138fc44cf47315df61cee9ce05ef5105
Paul Grace <pjgrace@gmail.com>
2022-05-17 12:04:46 +0100

    L4J remove
    Log4j removal

--- src/main/java/uk/ac/aston/opencomj/authentication/QuickStart.java
+++ src/main/java/uk/ac/aston/opencomj/authentication/QuickStart.java
@@ -26,8 +26,6 @@ import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.session.Session;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.util.Factory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 
 /**
@@ -37,7 +35,6 @@ import org.slf4j.LoggerFactory;
  */
 public class QuickStart {
 
-    private static final transient Logger log = LoggerFactory.getLogger(QuickStart.class);
 
 
     public static void main(String[] args) {
@@ -70,7 +67,7 @@ public class QuickStart {
         session.setAttribute("someKey", "aValue");
         String value = (String) session.getAttribute("someKey");
         if (value.equals("aValue")) {
-            log.info("Retrieved the correct value! [" + value + "]");
+            System.out.println("Retrieved the correct value! [" + value + "]");
         }
 
         // let's login the current user so we can check against roles and permissions:
@@ -80,11 +77,11 @@ public class QuickStart {
             try {
                 currentUser.login(token);
             } catch (UnknownAccountException uae) {
-                log.info("There is no user with username of " + token.getPrincipal());
+                System.out.println("There is no user with username of " + token.getPrincipal());
             } catch (IncorrectCredentialsException ice) {
-                log.info("Password for account " + token.getPrincipal() + " was incorrect!");
+                System.out.println("Password for account " + token.getPrincipal() + " was incorrect!");
             } catch (LockedAccountException lae) {
-                log.info("The account for username " + token.getPrincipal() + " is locked.  " +
+                System.out.println("The account for username " + token.getPrincipal() + " is locked.  " +
                         "Please contact your administrator to unlock it.");
             }
             // ... catch more exceptions here (maybe custom ones specific to your application?
@@ -95,28 +92,28 @@ public class QuickStart {
 
         //say who they are:
         //print their identifying principal (in this case, a username):
-        log.info("User [" + currentUser.getPrincipal() + "] logged in successfully.");
+        System.out.println("User [" + currentUser.getPrincipal() + "] logged in successfully.");
 
         //test a role:
         if (currentUser.hasRole("schwartz")) {
-            log.info("May the Schwartz be with you!");
+            System.out.println("May the Schwartz be with you!");
         } else {
-            log.info("Hello, mere mortal.");
+            System.out.println("Hello, mere mortal.");
         }
 
         //test a typed permission (not instance-level)
         if (currentUser.isPermitted("lightsaber:wield")) {
-            log.info("You may use a lightsaber ring.  Use it wisely.");
+            System.out.println("You may use a lightsaber ring.  Use it wisely.");
         } else {
-            log.info("Sorry, lightsaber rings are for schwartz masters only.");
+            System.out.println("Sorry, lightsaber rings are for schwartz masters only.");
         }
 
         //a (very powerful) Instance Level permission:
         if (currentUser.isPermitted("winnebago:drive:eagle5")) {
-            log.info("You are permitted to 'drive' the winnebago with license plate (id) 'eagle5'.  " +
+            System.out.println("You are permitted to 'drive' the winnebago with license plate (id) 'eagle5'.  " +
                     "Here are the keys - have fun!");
         } else {
-            log.info("Sorry, you aren't allowed to drive the 'eagle5' winnebago!");
+            System.out.println("Sorry, you aren't allowed to drive the 'eagle5' winnebago!");
         }
 
         //all done - log out!
